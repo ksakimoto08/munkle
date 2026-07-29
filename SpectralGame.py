ee36c2241dcc93b35e1a1dc869014a26b711dff3
ksakimoto08 <ksakimoto08@gmail.com>
2026-07-29 16:28:10 -0400

    Update SpectralGame.py

--- SpectralGame.py
+++ SpectralGame.py
@@ -14,7 +14,8 @@ from rdkit.Chem import Draw,rdRascalMCES
 from rdkit.Chem.Draw import rdMolDraw2D
 
 st.iframe(("https://lifescience.opensource.epam.com/KetcherDemoSA/index.html?hiddenControls="
-        "arom,dearom,cip,check,analyse,recognize,miew,reaction-plus,arrows,reaction-mapping-tools,rgroup,shape,text,images,enhanced-stereo,fullscreen,sgroup,open,save,paste")
+           "arom,dearom,cip,check,analyse,recognize,miew,reaction-plus,arrows,reaction-mapping-tools"
+           "rgroup,shape,text,images,enhanced-stereo,fullscreen,sgroup,open,save,paste"))
 
 #global display options
 structure_width = 300
